790830246c9fa25395d2a6cdff225415b9f2f28c
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-01-05 12:03:26 -0600

    Updated solutions + tests for js kata to ES6

--- javascript/multiples_of_3_or_5/solution.js
+++ javascript/multiples_of_3_or_5/solution.js
@@ -1,18 +1,17 @@
-
-function solution(number) {
-    let sum = 0
+const solution = (number) => {
+    let sum = 0;
     const mySet = new Set();
     for (let i = 1; i < number; i++) {
         if (mySet.has(i)) {
-            continue
+            continue;
         } else if (i % 3 === 0 && i % 5 === 0) {
-            mySet.add(i)
-            sum += i
+            mySet.add(i);
+            sum += i;
         } else if (i % 3 === 0 || i % 5 === 0) {
-            sum += i
+            sum += i;
         }
     }
-    return sum
-}
+    return sum;
+};
 
-module.exports = { solution };
+export { solution };
--- javascript/test/multiples_of_3_or_5.js
+++ javascript/test/multiples_of_3_or_5.js
@@ -1,18 +1,18 @@
-const { assert } = require("chai");
-const { solution } = require("../multiples_of_3_or_5/solution.js");
+import { assert } from "chai";
+import { solution } from "../multiples_of_3_or_5/solution.js";
 
 
-function test(n, expected) {
+const test = (n, expected) => {
   it(`n=${n}`, () => {
-    let actual = solution(n)
-    assert.strictEqual(actual, expected)
-  })
-}
+    let actual = solution(n);
+    assert.strictEqual(actual, expected);
+  });
+};
 
 
-describe("basic tests", function () {
-  test(10, 23)
-  test(-10, 0)
-  test(16, 60)
-  test(100, 2318)
-})
+describe("basic tests", () => {
+  test(10, 23);
+  test(-10, 0);
+  test(16, 60);
+  test(100, 2318);
+});
