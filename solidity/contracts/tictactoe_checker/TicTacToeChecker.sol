e1d8023a82d2327069d92c1cc2003b105bec7123
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-05-22 15:45:11 -0700

    Used new formatter to refactor previous contracts

--- solidity/contracts/tictactoe_checker/TicTacToeChecker.sol
+++ solidity/contracts/tictactoe_checker/TicTacToeChecker.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: BSD-2-Clause
 pragma solidity ^0.8.16;
 
-
 /// @title A checker for TicTacToe game
 /// @author Your Name
 /// @notice This contract checks if a TicTacToe game is solved and if a player has won
 contract TicTacToeChecker {
-
-  enum Player {None, X, O }
+  enum Player {
+    None,
+    X,
+    O
+  }
 
   /// @notice Determines the state of the TicTacToe game
   /// @param board The current state of the TicTacToe game board
-  /// @return The result of the game. 
+  /// @return The result of the game.
   ///   - 1 if player X won
   ///   - 2 if player O won
-  ///   - -1 if the game is not yet finished 
-  ///   - 0 if it's a draw, 
+  ///   - -1 if the game is not yet finished
+  ///   - 0 if it's a draw,
   function checkBoard(uint8[][] memory board) public pure returns (int8) {
-
     if (playerWon(Player.X, board)) {
       return 1;
     } else if (playerWon(Player.O, board)) {
@@ -33,19 +34,16 @@ contract TicTacToeChecker {
   /// @notice Determines if a player has won the TicTacToe game
   /// @param player The player to check
   /// @param board The current state of the TicTacToe game
-  /// @return true if the player has won, false otherwise
-  function playerWon(Player player, uint8[][] memory board) private pure returns (bool) {
-
+  /// @return won True if the player has won, false otherwise
+  function playerWon(Player player, uint8[][] memory board) private pure returns (bool won) {
     uint8 playerVal = uint8(player);
     uint8 verticalCount1 = 0;
     uint8 verticalCount2 = 0;
     uint8 verticalCount3 = 0;
     uint8 diagonalCount = 0;
     for (uint i = 0; i < 3; i++) {
-
       uint horizontalCount = 0;
       for (uint j = 0; j < 3; j++) {
-
         if (board[i][j] == playerVal) {
           if (i == j) {
             diagonalCount++;
@@ -60,7 +58,7 @@ contract TicTacToeChecker {
           }
 
           horizontalCount++;
-        } 
+        }
       }
 
       if (diagonalCount == 3) {
@@ -78,7 +76,7 @@ contract TicTacToeChecker {
 
     if (verticalCount1 == 3 || verticalCount2 == 3 || verticalCount3 == 3) {
       return true;
-    } 
+    }
   }
 
   /**
@@ -87,7 +85,6 @@ contract TicTacToeChecker {
    * @return A boolean indicating whether the board is not over.
    */
   function boardIsNotOver(uint8[][] memory board) private pure returns (bool) {
-    
     uint8 countOfUnusedCells = 0;
     for (uint i = 0; i < 3; i++) {
       for (uint j = 0; j < 3; j++) {
